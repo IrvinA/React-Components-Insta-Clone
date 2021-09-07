fa431b117ce36091834fdc97ebc869c79d4e72ba
IrvinA <arevalosirvin@gmail.com>
2021-09-07 14:43:54 -0700

    Assignment complete

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -8,22 +8,22 @@ import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
-  const { likePost, numberOfLikes } = props;
-
+  const { likePost, post } = props;
+  console.log(props);
   return (
     <div>
       <div
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div className='like-section-wrapper' onClick={event => likePost(post.id)}>
           <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{post.likes}</p>
     </div>
   );
 };
